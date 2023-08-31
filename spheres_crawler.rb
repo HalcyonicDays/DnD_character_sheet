d1aa53ae43aca3c253a26447d687e4096e5befa7
HalcyonicDays <white.zayetz@gmail.com>
2023-08-30 20:21:02 -0400

    Update spheres_crawler.rb

--- spheres_crawler.rb
+++ spheres_crawler.rb
@@ -7,11 +7,8 @@ SPHERES = %w(Alteration Conjuration Creation Dark Death Destruction
 	Protection Telekinesis Time Universal Warp Weather)
 
 class WebCrawler
-
 	def initialize()
 	end
-
-
 end
 
 class Nokogiri::XML::Element
@@ -24,7 +21,7 @@ end
 
 url = 'http://spheres5e.wikidot.com/spheres-of-power'
 doc = Nokogiri::HTML(URI.open(url))
-sphere_headings = doc.css('strong').select {|node| node.children.text =~ /\w*Spheres$/}.uniq
+sphere_headings = doc.css('strong').select {|node| node.children.text =~ /\w*Spheres$/}
 
 sphere_headings.each do |node|
 	puts node, node.children.text, node.next_element
